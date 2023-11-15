4e1e59269a9ac6e6ab39905b54fbe08afa18d2b8
liao1226 <40133999+liao1226@users.noreply.github.com>
2023-11-15 21:15:26 +0800

    Add files via upload

---
+++ DEMO/hello_world/hello_world.py
@@ -0,0 +1,18 @@
+"""
+File: hello_world.py
+Name: 
+-----------------------------------
+This program prints 'hello, world!'
+on Console and shows the basic 
+Python syntax
+"""
+
+
+def main():
+	pass
+
+
+### DO NOT EDIT THE CODE BELOW THIS LINE ###
+
+if __name__ == '__main__':
+	main()
